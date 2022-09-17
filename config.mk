535a5cf9594d1c92b20f865e81f9faddecca7a19
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-09-17 15:41:12 -0400

    Create config.mk

---
+++ config.mk
@@ -0,0 +1,16 @@
+# name of the target executable
+PROJECT =
+# project version (semver)
+VERSION =
+# name of the host platform
+PLATFORM =
+
+# paths
+INC_DIR = inc
+SRC_DIR = src
+LIB_DIR = lib
+BIN_DIR = bin
+VND_DIR = vendor
+DOC_DIR = docs
+MAN_DIR = man
+TST_DIR = test
