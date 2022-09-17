69f4cd4499ad166ff80fc452476a89b8f20c1e8a
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-09-17 15:57:28 -0400

    added library variables

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # name of the target executable
 PROJECT =
-# project version (semver)
+# project semver version
 VERSION =
 # name of the host platform
 PLATFORM =
@@ -14,3 +14,11 @@ VND_DIR = vendor
 DOC_DIR = docs
 MAN_DIR = man
 TST_DIR = test
+
+# name of the main library without prefixes or extensions
+LIB_NAME =
+# library extension
+LIB_EXT =
+# full name of the main library consisting of the library prefix, name, and
+# extension 
+LIB_FULL = lib$(LIB_NAME).$(LIB_EXT)
