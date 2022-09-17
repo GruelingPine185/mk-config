b962181dca2702a9585b38e3c277cb9d40a99024
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-09-17 18:55:07 -0400

    C/C++ compiler settings

--- config.mk
+++ config.mk
@@ -30,3 +30,27 @@ LIB_EXT =
 # full name of the main library consisting of the library prefix, name, and
 # extension 
 LIB_FULL = lib$(LIB_NAME).$(LIB_EXT)
+
+COMPILER =
+
+# C compiler
+CC =
+# C language version
+C_STD =
+# the C compiler optimization flag
+C_OPT =
+# space-separated warnings the C compiler will invoke
+C_WARN = -Wall -Wextra -pedantic
+# space-separated C preprocessor defines
+C_DEFS =
+# space-separate include paths
+C_INCLUDES = -I$(INC_DIR)
+# space-separated libraries to link to PROGRAM (e.g -lbase)
+C_LIBS =
+# space-sepatated paths to the libraries provided in C_LIBS
+C_LIB_PATHS =
+# the flags passed to C compiler
+C_FLAGS = $(C_STD) $(C_OPT) $(C_WARN)
+
+# C++ compiler
+CXX =
